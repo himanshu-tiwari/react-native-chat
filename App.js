b7dd5b03cc0630be88e7c6f2870cf325126b5a2e
himanshu-tiwari <htiwarih@gmail.com>
2020-06-09 13:56:08 +0530

    Added the Firebase context provider to the App

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { FireContextProvider } from './app/FireContext';
 const App = () => {
+	return <FireContextProvider>
+	</FireContextProvider>;
 };
 	
-
 export default App;
