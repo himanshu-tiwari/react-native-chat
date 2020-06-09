aee4e5055d4bb9263fa6755ad8dbbc2897ceacb1
himanshu-tiwari <htiwarih@gmail.com>
2020-06-09 14:22:48 +0530

    Added FireContext to the ChatScreen component

--- app/screens/ChatScreen/index.js
+++ app/screens/ChatScreen/index.js
@@ -1,10 +1,12 @@
 import React, { useState, useContext, useCallback, useEffect } from 'react';
 import { StyleSheet, Text, View, Platform, KeyboardAvoidingView } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat';
+import { FireContext } from '../../FireContext';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const ChatScreen = props => {
     const [messages, setMessages] = useState([]);
+    const { uid, get, db, off, send } = useContext(FireContext);
 
     const chat = <GiftedChat messages={messages} onSend={send} user={props.route?.params} />;
 
