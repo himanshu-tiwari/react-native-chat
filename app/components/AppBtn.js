a674fffdceb2fe8de90d9ecf8e2628fb4e768367
himanshu-tiwari <htiwarih@gmail.com>
2020-06-09 14:43:46 +0530

    CHanged the animation functions into callbacks using hooks

--- app/components/AppBtn.js
+++ app/components/AppBtn.js
@@ -1,29 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { TouchableWithoutFeedback, Animated, ActivityIndicator } from 'react-native';
 import { HiddenAppText } from './AppText';
 
 const AppBtn = props => {
     const [scale] = useState(new Animated.Value(1));
 
-    const animateIn = () => {
-        if (!props.disabled) {
-            Animated.timing(scale, {
-                toValue: props.scale || 0.85,
-                duration: props.duration || 200,
-                useNativeDriver: true
-            }).start();
-        }
-    };
+    const animateIn = useCallback(
+        () => {
+            if (!props.disabled) {
+                Animated.timing(scale, {
+                    toValue: props.scale || 0.85,
+                    duration: props.duration || 200,
+                    useNativeDriver: true
+                }).start();
+            }
+        },
+        [props.disabled, scale, props.scale, props.duration]
+    );
 
-    const animateOut = () => {
-        if (!props.disabled) {
-            Animated.timing(scale, {
-                toValue: 1,
-                duration: props.duration || 200,
-                useNativeDriver: true
-            }).start();
-        }
-    };
+    const animateOut = useCallback(
+        () => {
+            if (!props.disabled) {
+                Animated.timing(scale, {
+                    toValue: 1,
+                    duration: props.duration || 200,
+                    useNativeDriver: true
+                }).start();
+            }
+        },
+        [props.disabled, scale, props.duration]
+    );
 
     return <TouchableWithoutFeedback
         onPress={props.onPress}
