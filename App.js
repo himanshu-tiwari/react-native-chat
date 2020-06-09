26817e133c94eca6358d0ca01e63255b5457c595
himanshu-tiwari <htiwarih@gmail.com>
2020-06-09 14:04:38 +0530

    Added flash messaging to the project

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { FireContextProvider } from './app/FireContext';
+import FlashMessage from 'react-native-flash-message';
 import AppNavigation from './app/AppNavigation';
 
 const App = () => {
 	return <FireContextProvider>
 		<AppNavigation />
+
+		<FlashMessage position="top" />
 	</FireContextProvider>;
 };
 	
