39cd546dfae351606586745cf6bdce7bd2e6c5fa
himanshu-tiwari <htiwarih@gmail.com>
2020-06-10 14:58:16 +0530

    Memoized arrow functions using the useCallback hook in FireContext

--- app/FireContext.js
+++ app/FireContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useEffect, useState, useCallback } from 'react';
 import firebase from '@react-native-firebase/app';
 import '@react-native-firebase/auth';
 import '@react-native-firebase/firestore';
@@ -12,105 +12,125 @@ export const FireContextProvider = props => {
     const [db, setDb] = useState();
     const [uid, setUid] = useState();
 
-    const init = () => {
-        if (!firebase.apps.length) {
-            firebase.initializeApp(firebaseConfig);
-        }
-
-        setDb(firebase.firestore());
-    };
+    const init = useCallback(
+        () => {
+            if (!firebase.apps.length) {
+                firebase.initializeApp(firebaseConfig);
+            }
+    
+            setDb(firebase.firestore());
+        },
+        [firebase.apps.length],
+    );
 
     useEffect(() => {
         init();
     }, []);
 
-    const signIn = (credentials, callback) => {
-        firebase.auth().signInWithEmailAndPassword(
-            credentials.email,
-            credentials.password
-        ).then(res => {
-            showMessage({ type: "success", message: "Login successful!" });
-
-            if (typeof(callback) === "function") {
-                callback(res);
-            }
-        }).catch(error => {
-            showMessage({
-                type: "danger",
-                message: error?.message ? error?.message : "Unable to login! Check inputs!"
-            });
-            console.log(error);
-        });
-    };
+    const signIn = useCallback(
+        (credentials, callback) => {
+            firebase.auth().signInWithEmailAndPassword(
+                credentials.email,
+                credentials.password
+            ).then(res => {
+                showMessage({ type: "success", message: "Login successful!" });
     
-    const signOut = (callback) => {
-        firebase.auth().signOut().then(res => {
-            showMessage({ type: "success", message: "Logout successful!" });
-
-            if (typeof(callback) === "function") {
-                callback(res);
-            }
-        }).catch(error => {
-            showMessage({
-                type: "danger",
-                message: error?.message ? error?.message : "Error while logging out!"
+                if (typeof(callback) === "function") {
+                    callback(res);
+                }
+            }).catch(error => {
+                showMessage({
+                    type: "danger",
+                    message: error?.message ? error?.message : "Unable to login! Check inputs!"
+                });
+                console.log(error);
             });
-            console.log(error);
-        });
-    };
+        },
+        [],
+    );
     
-    const signUp = (newUser, callback) => {
-        firebase.auth().createUserWithEmailAndPassword(
-            newUser.email,
-            newUser.password
-        ).then(res => {
-            showMessage({ type: "success", message: "Signup successful!" });
-
-            if (typeof(callback) === "function") {
-                callback(res);
-            }
-        }).catch(error => {
-            showMessage({
-                type: "danger",
-                message: error?.message ? error?.message : "Unable to signup! Check inputs!"
+    const signOut = useCallback(
+        callback => {
+            firebase.auth().signOut().then(res => {
+                showMessage({ type: "success", message: "Logout successful!" });
+    
+                if (typeof(callback) === "function") {
+                    callback(res);
+                }
+            }).catch(error => {
+                showMessage({
+                    type: "danger",
+                    message: error?.message ? error?.message : "Error while logging out!"
+                });
+                console.log(error);
             });
-            console.log(error);
-        });
-    };
-
-    const send = messages => {
-        messages.forEach(message => {
-            db.collection("messages").add({
-                text: message.text,
-                timestamp: moment().format("X"),
-                user: message.user,
+        },
+        [],
+    );
+    
+    const signUp = useCallback(
+        (newUser, callback) => {
+            firebase.auth().createUserWithEmailAndPassword(
+                newUser.email,
+                newUser.password
+            ).then(res => {
+                showMessage({ type: "success", message: "Signup successful!" });
+    
+                if (typeof(callback) === "function") {
+                    callback(res);
+                }
+            }).catch(error => {
+                showMessage({
+                    type: "danger",
+                    message: error?.message ? error?.message : "Unable to signup! Check inputs!"
+                });
+                console.log(error);
             });
-        });
-    };
+        },
+        [],
+    );
 
-    const parse = message => {
-        console.log({ message: message.data() });
-        return ({
-            _id: message.id,
-            user: message.data()?.user,
-            text: message.data()?.text,
-            timestamp: moment(message.data()?.timestamp, "X").format("X"),
-            createdAt: moment(message.data()?.timestamp, "X")
-        });
-    };
+    const send = useCallback(
+        messages => {
+            messages.forEach(message => {
+                db.collection("messages").add({
+                    text: message.text,
+                    timestamp: moment().format("X"),
+                    user: message.user,
+                });
+            });
+        },
+        [db?.collection],
+    );
 
-    const get = callback => {
-        db.collection("messages").onSnapshot(querySnapshot => {
-            console.log("here");
-            callback(
-                querySnapshot.docs
-                    ?.map(parse)
-                    ?.sort((a, b) => moment(a?.createdAt).isBefore(b?.createdAt) ? 1 : -1)
-            );
-        });
-    };
+    const parse = useCallback(
+        message => {
+            console.log({ message: message.data() });
+            return ({
+                _id: message.id,
+                user: message.data()?.user,
+                text: message.data()?.text,
+                timestamp: moment(message.data()?.timestamp, "X").format("X"),
+                createdAt: moment(message.data()?.timestamp, "X")
+            });
+        },
+        [],
+    );
 
-    const off = () => db.off();
+    const get = useCallback(
+        callback => {
+            db.collection("messages").onSnapshot(querySnapshot => {
+                callback(
+                    querySnapshot.docs
+                        ?.map(parse)
+                        ?.sort((a, b) => moment(a?.createdAt).isBefore(b?.createdAt) ? 1 : -1)
+                );
+            });
+        },
+        [db?.collection, parse],
+    );
+
+    const off = useCallback(() => db.off(), [db?.off]);
 
     return <FireContext.Provider value={{ send, parse, get, off, db, uid, signIn, signOut, signUp }}>{
         props.children
