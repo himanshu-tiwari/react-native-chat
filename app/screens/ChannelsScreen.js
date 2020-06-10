33ea3c49c06ce9ff42e0cdf9b527f1935054910f
himanshu-tiwari <htiwarih@gmail.com>
2020-06-10 22:06:43 +0530

    Added custom map and filter methods for channels

--- app/screens/ChannelsScreen.js
+++ app/screens/ChannelsScreen.js
@@ -2,6 +2,8 @@ import React, { useContext, useState, useEffect, useCallback } from 'react';
 import { StyleSheet, View, SafeAreaView, ScrollView } from 'react-native';
 import AppText from '../components/AppText';
 import { FireContext } from '../FireContext';
+import { isNonEmptyArray } from '../helpers/checks';
+import AppBtn from '../components/AppBtn';
 
 const ChannelsScreen = props => {
     const { get } = useContext(FireContext);
@@ -11,6 +13,29 @@ const ChannelsScreen = props => {
     useEffect(() => {
         get("channels", setChannels);
     }, []);
+
+    const channelFilter = useCallback(
+        channel => channel?.members?.includes(props.route?.params?._id),
+        [],
+    );
+
+    const channelMap = useCallback(
+        channel => {
+            console.log({ channel: channel })
+            return <AppBtn onPress={() => {}}>
+                <AppText key={channel?._id}>{ channel?.title }</AppText>
+            </AppBtn>;
+        },
+        [],
+    );
+
+    return <SafeAreaView>
+        <ScrollView>{
+            isNonEmptyArray(channels)
+                ? channels.filter(channelFilter).map(channelMap)
+                : <AppText>No channel!</AppText>
+        }</ScrollView>
+    </SafeAreaView>;
 };
 
 export default ChannelsScreen;
