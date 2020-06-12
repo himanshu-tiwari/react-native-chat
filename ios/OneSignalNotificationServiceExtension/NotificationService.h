b6a51d2eeb29478d8e34139f8d034520ee53a246
himanshu-tiwari <htiwarih@gmail.com>
2020-06-12 14:52:18 +0530

    Updated xcode and android settings to receive notifications using onesignal

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Himanshu Tiwari on 12/06/20.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
